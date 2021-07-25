8cbf7aa284eabcdb202b23396738058daaf6b798
jsh <box6687@flow.team>
2021-07-25 13:36:44 +0900

    Feat: Add application class

---
+++ app/src/main/java/com/hunihun/usersearch/util/App.kt
@@ -0,0 +1,19 @@
+package com.hunihun.usersearch.util
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+    companion object {
+        lateinit var instance: App
+            private set
+    }
+
+}
