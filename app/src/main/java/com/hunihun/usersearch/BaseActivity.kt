f9a718de69c12253f78fcd968d08eef71b2f077b
sanghun <box6687@naver.com>
2021-07-30 01:31:02 +0900

    Feat: Add keyboard instance

--- app/src/main/java/com/hunihun/usersearch/BaseActivity.kt
+++ app/src/main/java/com/hunihun/usersearch/BaseActivity.kt
@@ -1,6 +1,8 @@
 package com.hunihun.usersearch
 
+import android.content.Context
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
@@ -11,6 +13,9 @@ abstract class BaseActivity<B: ViewDataBinding, VM: BaseViewModel>(
 
     abstract val vm: VM
     protected lateinit var binding: B
+    protected val imm by lazy(LazyThreadSafetyMode.NONE) {
+        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
