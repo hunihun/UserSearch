71ae5b8a1dd75fe00fcc6c8ba6eaf379334888f6
sanghun <box6687@naver.com>
2021-07-30 22:37:03 +0900

    Fix: Last push string format change

--- app/src/main/java/com/hunihun/usersearch/main/extension/UserExtension.kt
+++ app/src/main/java/com/hunihun/usersearch/main/extension/UserExtension.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.hunihun.usersearch.R
 import com.hunihun.usersearch.util.App
 import de.hdodenhof.circleimageview.CircleImageView
 import java.text.SimpleDateFormat
@@ -27,9 +28,11 @@ fun bindUserImage(view: CircleImageView, path: String?) {
 
 @BindingAdapter("bind_user_profile")
 fun bindUserProfile(view: TextView, data: String?) {
-    Log.d("jsh","data?.isNotEmpty() >>  " + data?.isNotEmpty())
-    val param = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-    param.setMargins(0, 10, 0, 0)
+    val param = LinearLayout.LayoutParams(
+        LinearLayout.LayoutParams.WRAP_CONTENT,
+        LinearLayout.LayoutParams.WRAP_CONTENT
+    )
+    param.setMargins(0, 30, 0, 0)
     view.apply {
         isVisible = data?.isNotEmpty() ?: false
         ellipsize = TextUtils.TruncateAt.END
@@ -43,10 +46,9 @@ fun bindUserProfile(view: TextView, data: String?) {
 fun bindUpdateTime(view: TextView, time: String) {
     if (time.isEmpty()) return
     //현재시간 Date
-    var curDate = Date(System.currentTimeMillis())
 
     //formater
-    val curDateFormat = SimpleDateFormat("yyyyMMddHHmm", Locale.getDefault())
+    val curDateFormat = SimpleDateFormat("yyyy.MM.dd", Locale.getDefault())
     val serverDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
 
     //요청시간을 Date로 parsing 후 time가져오기
@@ -54,5 +56,5 @@ fun bindUpdateTime(view: TextView, time: String) {
 
     val date = curDateFormat.format(reqDate)
 
-    view.text = "Last Push Date: " + date.substring(0, 4).toInt().toString() + "." + date.substring(4, 6).toInt().toString() + "." + date.substring(6, 8).toInt().toString()
+    view.text = String.format(App.instance.getString(R.string.last_update), date)
 }
